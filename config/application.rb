e77e92938b743c9d676a13a2b191e530d62362c6
ISMAIL YASSIN <YISMAIL299@GMAIL.COMssh-keygen -t rsa -C ISMAIL>
2016-10-10 14:01:24 +0200

    first commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Codaisseurbnb
+  class Application < Rails::Application
+    # ...
+
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
